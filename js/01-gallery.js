96b9f550ed134191867411221d68dfadd03e15e2
StealthM0de <jedongfallorina@gmail.com>
2023-11-12 03:00:39 +0800

    HW7 task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGallery = (el) => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+};
+
+// console.log(galleryItems);
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photosMarkup);
+
+const handleGalleryClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const urlOrignal = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${urlOrignal}">`);
+
+  instance.show();
+};
+
+galleryList.addEventListener("click", handleGalleryClick);
